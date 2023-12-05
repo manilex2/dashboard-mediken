6178ed351a68d05314e80e5d6b23e34650181675
Manuel Pereira <manilex2@gmail.com>
2023-12-05 17:58:37 -0500

    Optimizado el archivo environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,29 +1,10 @@
 export const environment = {
   production: false,
   serverURL: "URL DEL SERVIDOR EXPRESS",
-  msalConfig: {
-      auth: {
-          tenantId: 'TENANT_ID',
-          clientId: 'CLIENT_ID',
-          authority: 'DIRECCION_AUTORIDAD',
-          server: "DIRECCION_HOST",
-          groupId: "ID_AREA_TRABAJO"
-      }
-  },
   apiConfig: {
-      scopes: ['SCOPES'],
-      uri: 'URI_ACCESSO',
-      postLogoutUrl: 'POSTLOGOUT_URL',
-      serverTokenUrl: 'URL_SERVER_TOKEN'
+    serverTokenUrl: 'URL_SERVER_TOKEN'
   },
   powerbiConfig: {
-    tokenEndPoint: 'URL_TOKEN',
-    resource: 'RESOURCE_URL',
-    prompt: 'PROMPT',
-    reportId: 'ID_REPORTE',
-    embedUrl: 'EMBED_URL',
-    pbiUsername: 'USUARIO_POWERBI',
-    pbiPasswod: 'CONTRASEÑA_POWERBI',
     resumen: 'REPORT SECTION ID RESUMEN',
     brokers: 'REPORT SECTION ID BROKERS',
     brokersMediken: 'REPORT SECTION ID BROKERS MEDIKEN',
